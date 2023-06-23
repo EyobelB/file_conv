77345a4c2365da040e5ee4c307a9e50e4b55f76b
EyobelB <68450057+EyobelB@users.noreply.github.com>
2023-06-23 09:41:11 -0500

    Adding main path files

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"file/audio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Create file object for the song
+	francis, open_err := os.Open("src/francis_forever.flac")
+	if open_err != nil {
+		fmt.Println("Error: File opening failed....")
+	}
+
+	// Import "Francis Forever" by Mitski as a .flac object
+	francis_flac := audio.NewFlac(*francis)
+
+	// Continue with conversion from here
+	// For now, just print the magic number....
+	fmt.Printf("Magic number: %s\n", francis_flac.MagicNumber())
+	fmt.Println("Raw byte data: ", francis_flac.MagicNumber())
+	fmt.Printf("File is a valid .flac? %t\n", francis_flac.ValidFlac())
+	fmt.Println("Metadata incoming....")
+	francis_flac.PrintMetadata()
+}
